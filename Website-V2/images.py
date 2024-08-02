99428858dae7c58ccdd826170bdf575cfed80038
Caleb Schmitt <calebjschmitt2004@gmail.com>
2024-08-02 15:29:52 -0400

    V 1.5

---
+++ Website-V2/images.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, send_file
+import flask
+
+images = Blueprint("images", __name__)
+
+
+@images.route("/homepage/message")
+def homepage_message_image():
+    return send_file("./images/homepage-message.jpeg")
+
+
+@images.route("/about/me")
+def about_profile_image():
+    return send_file("./images/me.jpeg")
+
+
+@images.route("/website/icon")
+def icon():
+    return send_file("./images/icon.png")
+
+
+@images.route("/projects/data-tracker")
+def data_tracker():
+    return send_file("./images/data-tracker.png")
---
+++ Website-V2/website.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from views import views
+from images import images
+
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/")
+app.register_blueprint(images, url_prefix="/images")
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
